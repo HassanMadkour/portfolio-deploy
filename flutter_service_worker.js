4b3bf0661f74ad33d69897c2dcbd271beec6b3e9
HassanMadkour <hassan20elmsriy@gmail.com>
2024-11-09 23:37:45 +0200

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "510b07b13b6f8d79f20925c404fe57c0
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
-"flutter_bootstrap.js": "864a190d93f4655befaace0e0256acac",
+"flutter_bootstrap.js": "d651a3d9b38501cbfe1d5ffb013821b6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
