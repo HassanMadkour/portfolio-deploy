86806dfe7d41c1bd5dc7532e4514f28529e2388c
HassanMadkour <hassan20elmsriy@gmail.com>
2024-11-10 03:50:15 +0200

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2768344479"
+    serviceWorkerVersion: "3558153741"
   }
 });
